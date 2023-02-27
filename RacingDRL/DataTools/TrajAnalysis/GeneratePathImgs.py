f58a3fa3a7c46affed3cc17237a0521e82328a2d
Benjamin <benjaminevans316@gmail.com>
2023-02-27 15:21:10 +0200

    Added some data tools from TAL

--- RacingDRL/DataTools/TrajAnalysis/GeneratePathImgs.py
+++ RacingDRL/DataTools/TrajAnalysis/GeneratePathImgs.py
@@ -1,25 +1,22 @@
 from matplotlib import pyplot as plt
-# plt.rc('font', family='serif')
-# plt.rc('pdf',fonttype = 42)
-# plt.rc('text', usetex=True)
-plt.rcParams['pdf.use14corefonts'] = True
-
 import numpy as np
 import glob
 import os
 
+from PIL import Image
 import glob
+import trajectory_planning_helpers as tph
 from matplotlib.ticker import PercentFormatter
 from matplotlib.collections import LineCollection
 
 from RacingDRL.DataTools.MapData import MapData
-from RacingDRL.Planners.TrackLine import TrackLine 
+from RacingDRL.Planners.StdTrack import StdTrack 
+from RacingDRL.Planners.RacingTrack import RacingTrack
 from RacingDRL.Utils.utils import *
-from RacingDRL.DataTools.plotting_utils import *
 from matplotlib.ticker import MultipleLocator
 
-SAVE_PDF = False
-# SAVE_PDF = True
+# SAVE_PDF = False
+SAVE_PDF = True
 
 
 def ensure_path_exists(folder):
@@ -44,27 +41,27 @@ class AnalyseTestLapData:
         print(vehicle_folders)
         print(f"{len(vehicle_folders)} folders found")
 
-        set = 1
         for j, folder in enumerate(vehicle_folders):
             print(f"Vehicle folder being opened: {folder}")
-                
+            
             self.process_folder(folder)
 
     def process_folder(self, folder):
         self.path = folder
+        if not os.path.exists(self.path + "TestPaths/"): 
+            os.mkdir(self.path + "TestPaths/")    
 
         self.vehicle_name = self.path.split("/")[-2]
-        print(f"Vehicle name: {self.vehicle_name}")
-        self.map_name = self.vehicle_name.split("_")[3]
+        self.map_name = self.vehicle_name.split("_")[4]
+        if self.map_name == "f1":
+            self.map_name += "_" + self.vehicle_name.split("_")[5]
         self.map_data = MapData(self.map_name)
-        self.std_track = self.TrackLine(self.map_name, False)
-        self.racing_track = self.TrackLine(self.map_name, True)
+        self.std_track = StdTrack(self.map_name)
+        self.racing_track = RacingTrack(self.map_name)
 
-        if not os.path.exists(self.path + "TestingVelocities/"): 
-            os.mkdir(self.path + "TestingVelocities/")    
         for self.lap_n in range(5):
             if not self.load_lap_data(): break # no more laps
-            self.plot_velocity_heat_map()
+            self.generate_path_plot()
 
 
     def load_lap_data(self):
@@ -79,58 +76,46 @@ class AnalyseTestLapData:
 
         return 1 # to say success
 
-    
-    def plot_velocity_heat_map(self): 
-        save_path  = self.path + "TestingVelocities/"
-        
-        plt.figure(1)
+    def generate_path_plot(self):
+        plt.figure(1, figsize=(3, 3))
         plt.clf()
-        points = self.states[:, 0:2]
-        vs = self.states[:, 3]
-        
+
+        points = self.states[:, 0:2]        
         self.map_data.plot_map_img()
-
+        self.map_data.plot_centre_line()
         xs, ys = self.map_data.pts2rc(points)
-        points = np.concatenate([xs[:, None], ys[:, None]], axis=1)
-        points = points.reshape(-1, 1, 2)
-        segments = np.concatenate([points[:-1], points[1:]], axis=1)
-
-        norm = plt.Normalize(0, 8)
-        lc = LineCollection(segments, cmap='jet', norm=norm)
-        lc.set_array(vs)
-        lc.set_linewidth(5)
-        line = plt.gca().add_collection(lc)
-        cbar = plt.colorbar(line,fraction=0.046, pad=0.04, shrink=0.99)
-        cbar.ax.tick_params(labelsize=25)
-        plt.gca().set_aspect('equal', adjustable='box')
+        plt.plot(xs, ys, color='darkorange')
 
         plt.xticks([])
         plt.yticks([])
-        plt.tight_layout()
         ax = plt.gca()
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         ax.spines['bottom'].set_visible(False)
         ax.spines['left'].set_visible(False)
-        
-        name = save_path + f"{self.vehicle_name}_velocity_map_{self.lap_n}"
-        std_img_saving(name)
+
+        # txt = self.vehicle_name.split('_')[1]
+        # if len(txt)==3: txt = "Centre line"
+        # elif len(txt)==7: txt = "Racing line"
+        # plt.text(730, 90, txt, fontsize=11, ha='left', backgroundcolor='white', color='darkblue')
+
+        # plt.xlim(700, 1100)
+        # plt.ylim(60, 440)
+        plt.tight_layout()
+        # plt.show()
+
+        name = self.path + f"TestPaths/{self.vehicle_name}_velocity_map_{self.lap_n}"
+        plt.savefig(name + ".svg", bbox_inches='tight')
+        if SAVE_PDF:
+            plt.savefig(name + ".pdf", bbox_inches='tight', pad_inches=0)
 
 
-def esp_left_limits():
-    plt.xlim(20, 620)
-    plt.ylim(50, 520)
 
-def esp_right_limits():
-    plt.xlim(900, 1500)
-    plt.ylim(50, 520)
 
 def analyse_folder():
-
     p = "Data/"
 
-    path = p + "testPP_9/"
-    # path = p + "main_22"
+    path = p + "main_22"
     
     TestData = AnalyseTestLapData()
     TestData.explore_folder(path)
