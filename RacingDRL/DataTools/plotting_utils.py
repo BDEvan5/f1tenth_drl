4d74e3c15360575dd4dac2b8f96584425eca6b24
Benjamin <benjaminevans316@gmail.com>
2023-03-07 16:20:17 +0200

    Made training graph for games

--- RacingDRL/DataTools/plotting_utils.py
+++ RacingDRL/DataTools/plotting_utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 import glob
 
 
@@ -35,6 +36,73 @@ def load_time_data(folder, map_name=""):
                 means[keys[j]].append(float(lines[1].split(",")[1+j]))
 
     return mins, maxes, means
+
+
+def load_csv_data(path):
+    """loads data from a csv training file
+
+    Args:   
+        path (file_path): path to the agent
+
+    Returns:
+        rewards: ndarray of rewards
+        lengths: ndarray of episode lengths
+        progresses: ndarray of track progresses
+        laptimes: ndarray of laptimes
+    """
+    rewards, lengths, progresses, laptimes = [], [], [], []
+    with open(f"{path}training_data_episodes.csv", "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if float(row[2]) > 0:
+                rewards.append(float(row[1]))
+                lengths.append(float(row[2]))
+                progresses.append(float(row[3]))
+                laptimes.append(float(row[4]))
+
+    rewards = np.array(rewards)[:-1]
+    lengths = np.array(lengths)[:-1]
+    progresses = np.array(progresses)[:-1]
+    laptimes = np.array(laptimes)[:-1]
+    
+    return rewards, lengths, progresses, laptimes
+
+def true_moving_average(data, period):
+    if len(data) < period:
+        return np.zeros_like(data)
+    ret = np.convolve(data, np.ones(period), 'same') / period
+    for i in range(period): # start
+        t = np.convolve(data, np.ones(i+2), 'valid') / (i+2)
+        ret[i] = t[0]
+    for i in range(period):
+        length = int(round((i + period)/2))
+        t = np.convolve(data, np.ones(length), 'valid') / length
+        ret[-i-1] = t[-1]
+    return ret
+
+def convert_to_min_max_avg(step_list, progress_list, xs):
+    """Returns the 3 lines 
+        - Minimum line
+        - maximum line 
+        - average line 
+    """ 
+    n = len(step_list)
+
+    ys = np.zeros((n, len(xs)))
+    for i in range(n):
+        ys[i] = np.interp(xs, step_list[i], progress_list[i])
+
+    min_line = np.min(ys, axis=0)
+    max_line = np.max(ys, axis=0)
+    avg_line = np.mean(ys, axis=0)
+
+    return min_line, max_line, avg_line
+
+def smooth_line(steps, progresses, length_xs=300):
+    xs = np.linspace(steps[0], steps[-1], length_xs)
+    smooth_line = np.interp(xs, steps, progresses)
+
+    return xs, smooth_line
 
 
 
