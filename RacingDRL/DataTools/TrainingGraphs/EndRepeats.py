e2d55b538667f23ac9cc0c1f44e163c8270a3d9d
Benjamin <benjaminevans316@gmail.com>
2023-03-08 16:01:28 +0200

    tests

--- RacingDRL/DataTools/TrainingGraphs/EndRepeats.py
+++ RacingDRL/DataTools/TrainingGraphs/EndRepeats.py
@@ -9,38 +9,34 @@ from RacingDRL.DataTools.plotting_utils import *
 
 def make_TrainingGraph():
     base_path = "Data/"
-    test_name = "GameAblation" 
+    test_name = "EndMaps" 
     set_number = 3
-    speed = 4
     p = base_path + test_name + f"_{set_number}/"
 
     steps_list = []
     progresses_list = []
     
-    id_names = ["state", "waypoints", "lidar", "lidarWaypoints", "stateWaypoints", "stateLidar", "stateLidarWaypoints"]
-    print_names = ["S", "W", "L", "L+W", "S + W", "S + L", "S + L + W"]
-    # print_names = ["x", "W", "L", "LW", "stateWaypoints", "stateLidar", "stateLidarWaypoints"]
+    id_names = ["0", "1", "2"]
 
     n_repeats = 1
     for i, id_name in enumerate(id_names): 
         steps_list.append([])
         progresses_list.append([])
         for j in range(n_repeats):
-            path = p + f"AgentOff_SAC_GameAblation_esp_{id_name}_{speed}_{set_number}_{j}/"
+            path = p + f"AgentOff_SAC_endToEnd_aut_v6_6_{set_number}_{id_name}/"
             rewards, lengths, progresses, _ = load_csv_data(path)
             steps = np.cumsum(lengths[:-1]) / 1000
             avg_progress = true_moving_average(progresses[:-1], 20)
             steps_list[i].append(steps)
             progresses_list[i].append(avg_progress)
 
-    # plt.figure(2, figsize=(6, 3))
     plt.figure(2, figsize=(4.5, 2.3))
 
     xs = np.linspace(0, 20, 300)
     for i in range(len(steps_list)):
         min, max, mean = convert_to_min_max_avg(steps_list[i], progresses_list[i], xs)
-        plt.plot(xs, mean, '-', color=pp[i], linewidth=2, label=print_names[i])
-        # plt.gca().fill_between(xs, min, max, color=pp_dark[i], alpha=0.2)
+        plt.plot(xs, mean, '-', color=pp_dark[i], linewidth=2, label=id_names[i])
+        plt.gca().fill_between(xs, min, max, color=pp_dark[i], alpha=0.2)
 
 
     plt.gca().get_yaxis().set_major_locator(MultipleLocator(25))
